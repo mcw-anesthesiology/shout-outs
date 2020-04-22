cdf88ec71cce1e87c9ff2789d892b27749b9a1cd
Jacob Mischka <jacob@mischka.me>
2020-04-22 14:51:34 -0500

    Start on shoutouts plugin

---
+++ MCWAnesthShoutOuts.php
@@ -0,0 +1,118 @@
+<?php
+/*
+Plugin Name: MCW Anesthesiology Shout-outs
+Author: Jacob Mischka
+Version: 0.1.0
+License: GPL-3.0
+*/
+
+namespace MCWAnesthShoutOuts;
+
+defined ('ABSPATH') or die('No');
+
+require_once('ShoutOutController.php');
+
+use WP_Error;
+
+class MCWAnesthShoutOuts {
+	const API_NAMESPACE = 'mcw-anesth-shout-outs/v1';
+	const DB_NAMESPACE = 'mcw_anesth_shout_outs';
+
+	public function __construct() {
+		register_activation_hook(__FILE__, [$this, 'createTables']);
+
+		add_action('init', [$this, 'initPlugin']);
+		add_action('rest_api_init', [$this, 'initRestApi']);
+	}
+
+	const ALLOWED_ORIGINS = [];
+
+	static function matchesNamespace($route) {
+		return strpos($route, self::API_NAMESPACE) === 1;
+	}
+
+	public function initPlugin() {
+
+	}
+
+	static function extractUserData($user) {
+		return [
+			'id' => $user->ID,
+			'name' => $user->display_name
+		];
+	}
+
+	public function initRestApi() {
+		remove_filter('rest_pre_serve_request', 'rest_send_cors_headers');
+		add_filter('rest_pre_dispatch', function($result, $server, $request) {
+			$user = wp_get_current_user();
+			if (!$user || !$user->ID)
+				return new WP_Error('unauthorized', 'Unauthorized', ['status' => 401]);
+		}, 10, 4);
+
+		add_filter('rest_pre_serve_request', function($served, $response, $request, $server) {
+			if (self::matchesNamespace($request->get_route())) {
+				header('Access-Control-Allow-Credentials: true');
+				header('Access-Control-Allow-Methods: POST, GET, PATCH, DELETE');
+				header('Access-Control-Allow-Headers: Authorization, Content-Type, X-WP-Nonce');
+
+				$origin = get_http_origin();
+				$allowOrigin = esc_url_raw(site_url());
+
+				if ($origin && in_array($origin, self::ALLOWED_ORIGINS)) {
+					$allowOrigin = $origin;
+				}
+				header('Access-Control-Allow-Origin: ' . $allowOrigin);
+			}
+
+		}, 10, 4);
+
+		register_rest_route(self::API_NAMESPACE, '/user', [
+			'methods' => ['GET'],
+			'callback' => function($request) {
+				return self::extractUserData(wp_get_current_user());
+			}
+		]);
+		register_rest_route(self::API_NAMESPACE, '/users', [
+			'methods' => ['GET'],
+			'callback' => function($request) {
+				$users = get_users();
+				return array_map(self::extractUserData, array_values($users));
+			}
+		]);
+
+		register_rest_route(self::API_NAMESPACE, '/shoutouts', [
+			'methods' => ['GET', 'POST', 'PATCH', 'DELETE'],
+			'callback' => [ShoutOutController::class, 'handleRequest']
+		]);
+	}
+
+	public function createTables() {
+		global $wpdb;
+
+		$shoutouts = self::getTableName('shoutouts');
+
+		$charsetCollate = $wpdb->get_charset_collate();
+		require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+
+		$sql = "CREATE TABLE IF NOT EXISTS {$shoutouts} (
+			id bigint(20) NOT NULL AUTO_INCREMENT,
+			recipient_id bigint(20),
+			recipient_writein varchar(255),
+			message text NOT NULL,
+			created_by bigint(20) NOT NULL,
+			created_at datetime NOT NULL,
+			updated_at datetime NOT NULL,
+			PRIMARY KEY  (id)
+		) {$charsetCollate}";
+		dbDelta($sql);
+	}
+
+	public static function getTableName($name) {
+		global $wpdb;
+
+		return $wpdb->prefix . self::DB_NAMESPACE . '_' . $name;
+	}
+}
+
+$mcwAnesthShoutOuts = new MCWAnesthShoutOuts();
---
+++ ShoutOutController.php
@@ -0,0 +1,195 @@
+<?php
+
+namespace MCWAnesthShoutOuts;
+
+require_once('MCWAnesthShoutOuts.php');
+
+use WP_Error;
+
+class ShoutOutController {
+	const TABLE = 'shoutouts';
+	const COLUMNS = [
+		'id',
+		'recipient_id',
+		'recipient_writein',
+		'message',
+		'created_by',
+		'created_at',
+		'updated_at'
+	];
+	const REQUIRED = [
+		'message'
+	];
+	const JSON_COLUMNS = [];
+	const BOOLEAN_COLUMNS = [];
+
+	public static function handleRequest($request) {
+		switch ($request->get_method()) {
+		case 'GET':
+			return static::get($request);
+		case 'POST':
+			return static::post($request);
+		case 'PATCH':
+			return static::patch($request);
+		case 'DELETE':
+			return static::delete($request);
+		}
+	}
+
+	static function getParams($params) {
+		return
+			array_map(
+				function ($val) {
+					if (is_array($val)) {
+						return json_encode($val);
+					}
+
+					if (is_bool($val)) {
+						return $val ? 1 : 0;
+					}
+
+					return $val;
+				},
+				array_filter(
+					$params,
+					function ($key) {
+						return in_array($key, static::COLUMNS);
+					},
+					ARRAY_FILTER_USE_KEY
+				)
+			);
+	}
+
+	static function decodeJsonCols(&$row) {
+		foreach ($row as $key => $val) {
+			if (in_array($key, static::JSON_COLUMNS))
+				$row[$key] = json_decode($val);
+		}
+
+		return $row;
+	}
+
+	public static function get($request) {
+		global $wpdb;
+
+		$table = MCWAnesthShoutOuts::getTableName(static::TABLE);
+
+		$id = $request->get_param('id');
+		if (!empty($id)) {
+			$query = "SELECT * FROM {$table} WHERE id = %d";
+			$policy = $wpdb->get_row($wpdb->prepare($query, $id));
+
+			if (!empty($policy))
+				return self::decodeJsonCols($policy);
+
+			return new WP_Error('not-found', 'Not found', ['status' => 404]);
+		}
+
+		$results = $wpdb->get_results("SELECT * FROM {$table}", ARRAY_A);
+
+		if (!empty(static::JSON_COLUMNS)) {
+			foreach ($results as &$row) {
+				self::decodeJsonCols($row);
+			}
+		}
+
+		return $results;
+	}
+
+	public static function post($request) {
+		global $wpdb;
+
+		$params = $request->get_params();
+		foreach (static::REQUIRED as $param) {
+			if (empty($params[$param])) {
+				return new WP_Error('missing_params', 'Missing required parameters', ['status' => 400]);
+			}
+		}
+		if (empty($params['recipient_id']) && empty($params['recipient_writein'])) {
+			return new WP_Error('missing_params', 'Missing required parameters', ['status' => 400]);
+		}
+
+		if (!empty($params['recipient_id'])) {
+			$recipient = get_user_by('ID', $params['recipient_id']);
+
+			if (empty($recipient)) {
+				return new WP_Error('invalid-recipient', 'Recipient does not exist', ['status' => 404]);
+			}
+		}
+
+		$user = wp_get_current_user();
+		$table = MCWAnesthShoutOuts::getTableName(static::TABLE);
+		$wpdb->insert($table,
+			array_merge(
+				static::getParams($request->get_params()),
+				[
+					'created_by' => $user->ID,
+					'created_at' => date('c'),
+					'updated_at' => date('c')
+				]
+			)
+		);
+
+		$query = "SELECT * FROM {$table} WHERE id = %d";
+		return self::decodeJsonCols($wpdb->get_row($wpdb->prepare($query, [$wpdb->insert_id]), ARRAY_A));
+	}
+
+	public static function patch($request) {
+		global $wpdb;
+
+		$id = $request->get_param('id');
+
+		if (empty($id)) {
+			return new WP_Error('missing_params', 'Missing required parameters', ['status' => 400]);
+		}
+
+		$table = MCWAnesthShoutOuts::getTableName(static::TABLE);
+		$user = wp_get_current_user();
+
+		if (in_array('administrator', $user->roles)) {
+
+			$wpdb->update(
+				$table,
+				array_merge(
+					static::getParams($request->get_params()),
+					[
+						'updated_at' => date('c')
+					]
+				),
+				['id' => $id]
+			);
+		} else {
+			$wpdb->update(
+				$table,
+				array_merge(
+					static::getParams($request->get_params()),
+					[
+						'updated_at' => date('c')
+					]
+				),
+				['id' => $id, 'created_by' => $user->ID]
+			);
+
+		}
+
+		$query = "SELECT * FROM {$table} WHERE id = %d";
+		return self::decodeJsonCols($wpdb->get_row($wpdb->prepare($query, [$id]), ARRAY_A));
+	}
+
+	public static function delete($request) {
+		global $wpdb;
+
+		$id = $request->get_param('id');
+		if (empty($id))
+			return new WP_Error('missing_params', 'Missing required parameters', ['status' => 400]);
+
+		$table = MCWAnesthShoutOuts::getTableName(static::TABLE);
+		$user = wp_get_current_user();
+
+		if (in_array('administrator', $user->roles)) {
+			$wpdb->delete($table, ['id' => $id]);
+		} else {
+			$wpdb->delete($table, ['id' => $id, 'created_by' => $user->ID]);
+		}
+	}
+}
